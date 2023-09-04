2e281217112d3e6d2b3a7999e11cd54b068a0a62
Gauravshrivastav <107666289+gauravback@users.noreply.github.com>
2023-09-04 16:03:27 +0530

    forexfilter add

--- src/pages/Forex/Forex.jsx
+++ src/pages/Forex/Forex.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Offers from "@/components/Offers/Offers";
+import Filter from "@/components/SearchComponents/ForexFilter/ForexFilter";
 function Forex() {
   return (
+    <>
+    <Filter />
     <div className="mx-auto max-w-2xl pb-16 px-4 sm:px-6 lg:max-w-7xl lg:px-8">
       <Offers />
-    </div>
+    </div></>
   );
 }
 

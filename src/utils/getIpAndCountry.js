6200f8bac1ffe5d12493ce6eb3d64b45d4fec614
Rohan Phulkar <rohanphulkar936@gmail.com>
2023-09-04 18:37:51 +0530

    04 Sept 2023, homepage, navbar updated

--- src/utils/getIpAndCountry.js
+++ src/utils/getIpAndCountry.js
@@ -3,14 +3,16 @@ import axios from "axios";
 import { getCurrencyAndSymbolCode } from "./getCurrencyAndSymbolCode";
 import { useSelector } from "react-redux";
 import { add } from "@/redux/slices/IPSlice";
-
+const agent = new https.Agent({  
+  rejectUnauthorized: false
+});
 async function getIpAndCountry(dispatch, ipAddress = null) {
   try {
     const response = await axios.get("https://api.ipify.org?format=json");
     const ip = response.data.ip;
 
     if (!ip || ip != ipAddress) {
-      const countryResponse = await axios.get(`http://ip-api.com/json/${ip}`);
+      const countryResponse = await axios.get(`http://ip-api.com/json/${ip}`,{ httpsAgent: agent });
       const country = countryResponse.data.country;
       const { currency, symbolCode, abbreviation } = getCurrencyAndSymbolCode(
         country,
